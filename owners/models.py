b0f6aa58f420d4f119e5b3890ac261e0d467f140
bello yusuf <belloyusuf1997@gmail.com>
2023-11-25 11:35:41 +0100

    Done with owners model

--- owners/models.py
+++ owners/models.py
@@ -12,11 +12,33 @@ class TimeModels(models.Model):
 
 
 
-
+# Owners models 
 class Owners(TimeModels):
     SEX_TYPE = (
         ("Male", "Male"),
         ("Female", "Female")
     )
+    MARITAL_STATUS = (
+        ("Single", "Single"),
+        ("Married", "Married"),
+        ("Divorced", "Divorced")
+    )
+    OCCUPATION = (
+        ("Student", "Student"),
+        ("Civil Servant", "Civil Servant"),
+        ("Business", "Business")
+    )
+
     full_name = models.CharField(max_length=20)
     sex = models.CharField(choices=SEX_TYPE ,max_length=50)
+    marital_status = models.CharField(choices=MARITAL_STATUS ,max_length=50)
+    occupation = models.CharField(choices=OCCUPATION, max_length=50)
+    date_of_birth = models.DateField(auto_now=False, auto_now_add=False)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=254)
+    residential_address = models.CharField(max_length=50)
+    state = models.CharField(max_length=15)
+    nationality = models.CharField(max_length=15)
+
+
+
