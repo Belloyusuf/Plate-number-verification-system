8c62814bea240fb7dc606310146a0cd2c1f37937
bello yusuf <belloyusuf1997@gmail.com>
2023-11-25 11:27:09 +0100

    Working on car owner model

--- owners/models.py
+++ owners/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-class
+
+
+class TimeModels(models.Model):
+    """ Automatic created and updated date and time"""
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    class Meta:
+        abstract = True
+
+
+
+
+class Owners(TimeModels):
+    SEX_TYPE = (
+        ("Male", "Male"),
+        ("Female", "Female")
+    )
+    full_name = models.CharField(max_length=20)
+    sex = models.CharField(choices=SEX_TYPE ,max_length=50)
