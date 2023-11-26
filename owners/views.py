402355053e30145c868c560ab6be4c2b2b13d5ea
bello yusuf <belloyusuf1997@gmail.com>
2023-11-26 03:45:28 +0100

    working on car registration views.

--- owners/views.py
+++ owners/views.py
@@ -43,3 +43,36 @@ class OwnerRegistrationsDisplayViews(DetailView):
     model = Owner
     context_object_name = "owners"
     template_name = "content/detail.html"
+
+
+
+
+# Car registration list views
+class CarRegistrationViews(ListView):
+    model = CarRegisteration
+    context_object_name = "cars"
+    template_name = "content/car_list.html"
+
+
+#  Car Registration create views
+class CarRegistrationCreateView(SuccessMessageMixin, CreateView):
+    model = CarRegisteration
+    fields = "__all__"
+    context_object_name = "form"
+    success_url = "/dashboard/"
+    success_message = "%(vehicle_type)s was created successfully"
+    template_name = "content/car_create.html"
+
+
+# Car Registration Update Views
+class CarRegistrationUpdateViews(SuccessMessageMixin, UpdateView):
+    model = CarRegisteration
+    fields = "__all__"
+    context_object_name = "cars"
+    template_name = "content/car_update.html"
+
+
+
+# Car Registration Detail Views
+class CarRegistrationDetailViews(DetailView):
+    model = 
