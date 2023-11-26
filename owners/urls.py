b0b7e2d5e82e7efc7b163a1d10ca441392c7ac9c
bello yusuf <belloyusuf1997@gmail.com>
2023-11-26 04:05:26 +0100

    Done with urls

--- owners/urls.py
+++ owners/urls.py
@@ -5,5 +5,15 @@ from . views import OwnerRegistrationListViews, OwnerRegistrationsDisplayViews, 
                     CarRegistrationUpdateViews, CarRegistrationListViews
 
 urlpatterns = [
+    # Owner urls
     path("", views.dashboard, name="dashboard"),
+    path("owner-list/", OwnerRegistrationListViews.as_view(), name="owner-list"),
+    path("owner-create/", OwnerRegistrationViews.as_view(), name="owner-create"),
+    path("owner-update/<int:pk>/owner/", OwnerRegistrationsUpdateViews.as_view(), name="owner-update"),
+    path("owner-detail/<int:pk>/owner/", OwnerRegistrationsDisplayViews.as_view(), name="owner-detail"),
+    # Car details urls
+    path("car-list/", CarRegistrationListViews.as_view(), name="car-list"),
+    path("car-create/", CarRegistrationCreateView.as_view(), name="car-create"),
+    path("car-update/", CarRegistrationUpdateViews.as_view(), name="car-update"),
+    path("car-detail/", CarRegistrationDetailViews.as_view(), name="")
 ]
